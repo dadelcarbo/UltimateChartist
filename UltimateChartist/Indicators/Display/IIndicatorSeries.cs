c5167695bad7332d0522c6e38c3a47824824d513
dadelcarbo <david.carbonel@free.fr>
2023-03-09 17:03:39 +0100

    Initial commit

---
+++ UltimateChartist/Indicators/Display/IIndicatorSeries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UltimateChartist.Indicators.Display;
+
+public abstract class IndicatorValueBase
+{
+    public DateTime Date { get; set; }
+}
+
+public interface IIndicatorSeries
+{
+    public IEnumerable<IndicatorValueBase> Values { get; set; }
+}
---
+++ UltimateChartist/UserControls/InstrumentControls/InstrumentWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Instrument = UltimateChartist.DataModels.Instrument;
+
+namespace UltimateChartist.UserControls.InstrumentControls
+{
+    /// <summary>
+    /// Interaction logic for InstrumentWindow.xaml
+    /// </summary>
+    public partial class InstrumentWindow : Window
+    {
+        public InstrumentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadGridView_SelectionChanged(object sender, Telerik.Windows.Controls.SelectionChangeEventArgs e)
+        {
+            MainWindowViewModel.Instance.CurrentChartView.Instrument = e.AddedItems[0] as Instrument;
+        }
+    }
+}
